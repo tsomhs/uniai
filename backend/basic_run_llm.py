e82dbafccb2542d763cd2403b41357910dd51b6d
Pantelis Naoum <naoump@outlook.com>
2026-05-16 10:12:13 +0300

    Sample script to run a basic llm

---
+++ backend/basic_run_llm.py
@@ -0,0 +1,32 @@
+import os
+from openai import AzureOpenAI
+
+endpoint = "https://naoum-mordj9zw-eastus2.cognitiveservices.azure.com/"
+model_name = "gpt-5.4"
+deployment = "gpt-5.4-2"
+
+subscription_key = "<your-api-key>"
+api_version = "2024-12-01-preview"
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I am going to Paris, what should I see?",
+        }
+    ],
+    max_completion_tokens=16384,
+    model=deployment
+)
+
+print(response.choices[0].message.content)
